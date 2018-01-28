25aec08a6ba7b75db108f46d4f0655596df837c4
Jawad Shuja <jawad.shuja@gmail.com>
2018-01-28 17:04:17 +0500

    Package: Add style dependencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reducer from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
+import './assets/styles/index.scss';
 
 export const store = createStore(reducer);
 
